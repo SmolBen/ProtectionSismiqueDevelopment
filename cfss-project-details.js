b26740cca2e88fb560eb0ea5a5c1e2a724f29dd7
SmolBen <144072315+SmolBen@users.noreply.github.com>
2025-12-17 19:52:11 -0500

    fixed mÂ² character

--- cfss-project-details.js
+++ cfss-project-details.js
@@ -6674,7 +6674,7 @@ function displayCFSSData(cfssData) {
                             <tr style="background: #17a2b8; color: white;">
                                 <th style="padding: 8px; text-align: left;">Étage</th>
                                 <th style="padding: 8px; text-align: center;">H (m)</th>
-                                <th style="padding: 8px; text-align: center;">A (mÂ²)</th>
+                                <th style="padding: 8px; text-align: center;">A (m²)</th>
                                 <th style="padding: 8px; text-align: center;">ULS (psf)</th>
                                 <th style="padding: 8px; text-align: center;">SLS (psf)</th>
                             </tr>
